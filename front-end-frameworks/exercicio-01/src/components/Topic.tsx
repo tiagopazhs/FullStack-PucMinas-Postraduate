2ee5bd09f5e197e776adab09c6069bfcefc25e93
tiagopazhs <tiagopaz.hs@gmail.com>
2023-04-22 23:52:50 -0300

    fix: Add padding to the Topic component

--- front-end-frameworks/exercicio-01/src/components/Topic.tsx
+++ front-end-frameworks/exercicio-01/src/components/Topic.tsx
@@ -6,8 +6,8 @@ interface Props {
 
 const Topic: React.FC<Props> = ({ title }) => {
   return (
-    <li>
-      <a href="Topic 1" className="text-gray-800 hover:text-gray-600">{title}</a>
+    <li className='p-2'>
+      <a href="Topic 1" className="text-sm text-gray-800 hover:text-gray-600">{title}</a>
     </li>
   );
 };
