0b6e5543d809b21ec7cf5353ef2572b53d5d02f6
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-13 23:57:40 -0300

    feat: Add exercise five

---
+++ back-end-python/exercicio-1/question-5.py
@@ -0,0 +1,7 @@
+name = input("Digite seu nome: ")
+age = int(input("Digite sua idade: "))
+
+if age >= 18:
+    print(f"{name} é maior de 18 e tem {age} anos.")
+else:
+    print(f"{name} não é maior de 18 e tem {age} anos.")
