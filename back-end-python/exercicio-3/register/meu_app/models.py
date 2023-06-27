69e13f70160fd813cf9e59b0c5343ce46421cba8
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-26 23:19:39 -0300

    feat: Add model

--- back-end-python/exercicio-3/register/meu_app/models.py
+++ back-end-python/exercicio-3/register/meu_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    endereco = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=20)
+    curso = models.CharField(max_length=100)
