5131aa21bd826d959395051dc3c5e5a9040a7f86
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-26 23:04:07 -0300

    feat: Register helloWord url

--- back-end-python/exercicio-3/register/register/urls.py
+++ back-end-python/exercicio-3/register/register/urls.py
@@ -20,3 +20,10 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import path
+from meu_app.views import hello_world
+
+urlpatterns = [
+    path('hello/', hello_world),
+]
