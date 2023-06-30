970b8f28e50ddb45bcf612ad51c0d81572834311
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-30 14:52:45 -0300

    feat: Remove files to restart project

--- back-end-python/trabalho-pratico/easy_call/atendimentos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Atendimento, Mensagem, Cliente, Atendente
-
-admin.site.register(Atendimento)
-admin.site.register(Mensagem)
-admin.site.register(Cliente)
-admin.site.register(Atendente)
--- back-end-python/trabalho-pratico/easy_call/easy_call/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-    path('api/atendimentos/', 'easy_call.views.AtendimentoListAPIView.as_view()'),
-]
