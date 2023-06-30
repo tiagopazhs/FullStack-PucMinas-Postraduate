a4830545273db99be96ee4ca2e0d671c5e63eaa7
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-30 14:59:19 -0300

    feat: Add initial models an settings

--- back-end-python/final-task/easy_call/atendimentos/models.py
+++ back-end-python/final-task/easy_call/atendimentos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Atendimento(models.Model):
+    cliente = models.CharField(max_length=100)
+    atendente = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"Atendimento {self.pk}"
+
+class Mensagem(models.Model):
+    atendimento = models.ForeignKey(Atendimento, on_delete=models.CASCADE)
+    remetente = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    data_envio = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Mensagem {self.pk} - {self.remetente}"
