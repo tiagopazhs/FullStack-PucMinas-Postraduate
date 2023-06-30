25283254ee1e03a5596a3f9309bf94af07379215
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-30 15:26:52 -0300

    feat: Add views and urls to deal with crud templates

--- back-end-python/final-task/easy_call/atendimentos/views.py
+++ back-end-python/final-task/easy_call/atendimentos/views.py
@@ -4,3 +4,43 @@ from .models import Atendimento
 def listar_atendimentos(request):
     atendimentos = Atendimento.objects.all()
     return render(request, 'atendimentos/atendimentos.html', {'atendimentos': atendimentos})
+
+def criar_atendimento(request):
+    if request.method == 'POST':
+        form = AtendimentoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_atendimentos')
+    else:
+        form = AtendimentoForm()
+    return render(request, 'atendimentos/criar_atendimento.html', {'form': form})
+
+from django.shortcuts import render, get_object_or_404
+from .models import Atendimento
+
+def detalhar_atendimento(request, atendimento_id):
+    atendimento = get_object_or_404(Atendimento, pk=atendimento_id)
+    return render(request, 'atendimentos/detalhar_atendimento.html', {'atendimento': atendimento})
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import AtendimentoForm
+from .models import Atendimento
+
+def atualizar_atendimento(request, atendimento_id):
+    atendimento = get_object_or_404(Atendimento, pk=atendimento_id)
+    if request.method == 'POST':
+        form = AtendimentoForm(request.POST, instance=atendimento)
+        if form.is_valid():
+            form.save()
+            return redirect('detalhar_atendimento', atendimento_id=atendimento.id)
+    else:
+        form = AtendimentoForm(instance=atendimento)
+    return render(request, 'atendimentos/atualizar_atendimento.html', {'form': form, 'atendimento': atendimento})
+
+from django.shortcuts import get_object_or_404, redirect
+from .models import Atendimento
+
+def excluir_atendimento(request, atendimento_id):
+    atendimento = get_object_or_404(Atendimento, pk=atendimento_id)
+    atendimento.delete()
+    return redirect('listar_atendimentos')
--- back-end-python/final-task/easy_call/easy_call/urls.py
+++ back-end-python/final-task/easy_call/easy_call/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from atendimentos.views import listar_atendimentos
+from atendimentos.views import criar_atendimento, listar_atendimentos, detalhar_atendimento, atualizar_atendimento, excluir_atendimento
 
 urlpatterns = [
     path('atendimentos/', listar_atendimentos, name='listar_atendimentos'),
+    path('atendimentos/criar/', criar_atendimento, name='criar_atendimento'),
+    path('atendimentos/<int:atendimento_id>/', detalhar_atendimento, name='detalhar_atendimento'),
+    path('atendimentos/<int:atendimento_id>/atualizar/', atualizar_atendimento, name='atualizar_atendimento'),
+    path('atendimentos/<int:atendimento_id>/excluir/', excluir_atendimento, name='excluir_atendimento'),
 ]
