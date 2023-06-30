61aa52e6dd7fd0654d9d9e4497746251b8a68443
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-29 23:32:46 -0300

    fix: Urls errors

--- back-end-python/trabalho-pratico/easy_call/easy_call/urls.py
+++ back-end-python/trabalho-pratico/easy_call/easy_call/urls.py
@@ -1,9 +1,6 @@
-from django.urls import include, path
 from django.urls import path
 from .views import AtendimentoListAPIView
 
 urlpatterns = [
     path('api/atendimentos/', AtendimentoListAPIView.as_view(), name='atendimentos-list'),
 ]
-
-
