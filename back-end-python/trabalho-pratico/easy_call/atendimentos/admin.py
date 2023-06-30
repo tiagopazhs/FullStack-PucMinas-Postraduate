23231342dbdd3438aeff69de494c9a6895788aeb
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-29 23:28:00 -0300

    feat: Create admin

--- back-end-python/trabalho-pratico/easy_call/atendimentos/admin.py
+++ back-end-python/trabalho-pratico/easy_call/atendimentos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Atendimento, Mensagem, Cliente, Atendente
 
-# Register your models here.
+admin.site.register(Atendimento)
+admin.site.register(Mensagem)
+admin.site.register(Cliente)
+admin.site.register(Atendente)
