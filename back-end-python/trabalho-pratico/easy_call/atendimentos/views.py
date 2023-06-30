0fb295765d33c4f13bd437fcaa6a6cc41d5c94bb
tiagopazhs <tiagopaz.hs@gmail.com>
2023-06-29 23:27:36 -0300

    feat: Create views

--- back-end-python/trabalho-pratico/easy_call/atendimentos/views.py
+++ back-end-python/trabalho-pratico/easy_call/atendimentos/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Atendimento
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Atendimento
+from .serializers import AtendimentoSerializer
+
+
+def atendimentos_list(request):
+    atendimentos = Atendimento.objects.all()
+    data = {
+        'atendimentos': list(atendimentos.values())
+    }
+    return JsonResponse(data)
+
+class AtendimentoListAPIView(APIView):
+    def get(self, request):
+        atendimentos = Atendimento.objects.all()
+        serializer = AtendimentoSerializer(atendimentos, many=True)
+        return Response(serializer.data)
