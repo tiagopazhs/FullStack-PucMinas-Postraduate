8750200f8c5245b3dc8dedede2481d9efddc47fd
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-09 22:44:34 -0300

    feat: Add last version of greet tests

--- tests-qualidade-soft/examples/aula-1/greeting-app/greeting.js
+++ tests-qualidade-soft/examples/aula-1/greeting-app/greeting.js
@@ -1,11 +1,11 @@
 const greet = (name)  => {
 
-    // if (name.length === '') {
-    //     throw 'Name cannot be empty'
-    // }
-
     if (name === undefined) {
         throw 'Name cannot be undefined'
+    }
+
+    if (name.length === 0) {
+        throw 'Name cannot be empty'
     }
 
     return `Hello ${name}, this is Brazil`
--- tests-qualidade-soft/examples/aula-1/greeting-app/greeting.test.js
+++ tests-qualidade-soft/examples/aula-1/greeting-app/greeting.test.js
@@ -1,16 +1,19 @@
 const greet = require('./greeting')
 
 describe("Greeting", () => {
+
     test("Greeting the name specified name", () => {
         const message = greet('Toretto');
         expect(message).toBe('Hello Toretto, this is Brazil');
     });
+
     test("Greeting an empty name should return an exception", () => {
-        const expression = () => greet('');
-        expect(expression).toThrow("Greet don't allow empty names");
+        const message = () => greet('');
+        expect(message).toThrow('Name cannot be empty');
     });
+    
     test("Greeting an undefined name should return an exception", () => {
-        const expression = () => greet(undefined);
-        expect(expression).toThrow("Greet don't allow undefined names");
+        const message = () => greet(undefined);
+        expect(message).toThrow('Name cannot be undefined');
     });
 });
