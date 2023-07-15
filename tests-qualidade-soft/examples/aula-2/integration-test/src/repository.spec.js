d89262d4b24aead6cdb98c13c4595b67079866b0
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-15 17:57:00 -0300

    feat: Add test of Read event

--- tests-qualidade-soft/examples/aula-2/integration-test/src/repository.spec.js
+++ tests-qualidade-soft/examples/aula-2/integration-test/src/repository.spec.js
@@ -27,7 +27,34 @@ describe("EventRepository", () => {
         expect(events.length).toBe(1);
     
     })
-    test.todo('Respository must show all events (R)');
+    test('Respository must show all events (R)', async () => {
+
+        const dsn = 'mongodb://root:root@localhost?retryWrites=true&writeConcern=majority'
+        client = new MongoClient(dsn);
+        await client.connect();
+        const collection = client.db('app_db').collection('events');
+        
+        const repository = new EventRepository(collection);
+
+        await repository.create({
+            name: 'Rock in Rio',
+            date: '2024-02-07'
+        })
+
+        const result = await repository.findAll();
+
+        expect(result.length).toBe(1);
+
+        expect(result.length).toStrictEqual(
+            expect.objectContaining([
+                {
+                    name: 'Rock in Rio',
+                    date: '2024-02-07'
+                }
+            ])
+        )
+
+    });
     test.todo('Respository must update an event (U)');
     test.todo('Respository must delete an event (D)');
 });
