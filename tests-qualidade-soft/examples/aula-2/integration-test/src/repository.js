fa9cef91b26e48554e235a0b7de92b36b8d874e4
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-16 22:16:04 -0300

    config: Add folder to reWrite tests withOut class

---
+++ tests-qualidade-soft/examples/aula-2/integration-test/src/repository.js
@@ -0,0 +1,43 @@
+const mongo = require('mongodb');
+
+class EventRepository {
+
+    collection;
+
+    constructor(collection) {
+        this.collection = collection;
+    }
+
+    async deleteAll() {
+        await this.collection.deleteMany({});
+    }
+
+    async create(event) {
+        await this.collection.insertOne(event);
+        return event;
+    }
+
+    async findAll() {
+        return (await this.collection.find({})).toArray();
+    }
+
+    async update(event) {
+        await this.collection.updateOne({_id: event._id}, {$set: event});
+    }
+
+    async findById(id) {
+        return await this.collection.findOne({_id: new mongo.ObjectId(id)});
+    }
+
+    async delete(event) {
+
+        if (event._id === undefined) {
+            throw new Error('Invalid event');
+        }
+
+        await this.collection.deleteOne({_id: event._id});
+    }
+    
+}
+
+module.exports = EventRepository;
