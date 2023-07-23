65729e4779ec1c434254816adfe3bd9d36d75a0e
tiagopazhs <tiagopaz.hs@gmail.com>
2023-07-23 18:27:20 -0300

    feat: Add simple bdd implementation

---
+++ tests-qualidade-soft/examples/aula-5/my-draft/features/support/steps.js
@@ -0,0 +1,18 @@
+const { When, Then, Given, Before } = require('cucumber');
+const assert = require('assert');
+const app = require('../../src/app');
+const container = app.get('container');
+const request = require('supertest')(app);
+const mongo = require("mongodb");
+
+When('I send a {string} request to {string}', async (method, path) => {
+    this.response = await request[method.toLowerCase()](path);
+});
+
+Then('I should receive response code {int}', (statusCode) => {
+    assert.equal(this.response.status, statusCode)
+});
+
+Then('the JSON response should be equal to:', (responseBody) => {
+    assert.equal(JSON.stringify(this.response.body), JSON.stringify(JSON.parse(responseBody)));
+});
